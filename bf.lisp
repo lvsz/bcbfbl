1fffa780b30f4f2150222e0b509fa9f6b04442d8
lvsz <e@levi.si>
2016-10-03 23:40:37 +0200

    Refactored to actually fit on a business card

--- bf.lisp
+++ bf.lisp
@@ -1,8 +1,10 @@
-(defmacro !(&rest b)`(cons(lambda(),@b)(f))) ; Levi Siuzdak - 2016
-(let((~(open(cadr *posix-argv*)))(b(make-array 30000))(f 0))(defun
-?()(aref b f))(defun b(f)(mapc #'(lambda(f)(typecase f(function f(
-funcall f))(cons(if(>(?)0)(b f)))(null(exit))))f)(if(>(?)0)(b f)))
-(defun f()(case(read-char ~'())(#\<(!(decf f)))(#\>(!(incf f)))(
-#\+(!(incf(aref b f))))(#\-(!(decf(aref b f))))(#\,(!(setf(aref b
-f)(char-code(read-char'()'()#\U0)))))(#\.(!(princ(code-char(?)))(
-force-output)))(#\[(cons(f)(f)))(#\]'($))('()'(()))(t(f))))(b(f)))
+(defmacro !(&rest b)`(cons(lambda(),@b)(f)))
+(let((b(make-array 30000))(f 0) ;; Levi Siuzdak - 2016
+(~(open(cadr *posix-argv*))))(defun ?()(aref b f))(defun
+b(f)(mapc #'(lambda(f)(typecase f(function f(funcall f))
+(cons(if(>(?)0)(b f)))))f)(if(>(?)0)(b f)))(defun f()
+(case(read-char ~'())(#\<(!(decf f)))(#\>(!(incf f)))
+(#\+(!(incf(aref b f))))(#\-(!(decf(aref b f))))(#\.
+(!(princ(code-char(?)))(force-output)))(#\,(!(setf
+(aref b f)(char-code(read-char'()'()#\U0)))))(#\[
+(cons(f)(f)))(#\]'())('()`(,#'exit))(t(f))))(b(f)))
